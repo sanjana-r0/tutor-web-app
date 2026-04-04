0502797b34a6e158f8cf3b626b333429f512033d
sanjana-r0 <230701285@rajalakshmi.edu.in>
2026-04-04 12:50:17 +0530

    Import and expose login functions in app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,9 @@
+import { doLogin, setRole } from './auth.js';
+import { renderTutorGrid } from './student.js';
 
+window.doLogin = doLogin;
+window.setRole = setRole;
+
+window.onload = () => {
+  console.log("App loaded");
+};
