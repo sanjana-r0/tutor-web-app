2e06424e802e50e8626003299b2b4648e326ae98
sanjana-r0 <230701285@rajalakshmi.edu.in>
2026-04-04 14:44:22 +0530

    Add GetBookings function to retrieve bookings

--- api/GetBookings/index.js
+++ api/GetBookings/index.js
@@ -1,1 +1,16 @@
+module.exports = async function (context, req) {
 
+  const bookings = [
+    {
+      tutorName: "Dr. Rajan Nair",
+      subject: "Math",
+      date: "2026-04-15",
+      time: "9:00 AM",
+      status: "confirmed"
+    }
+  ];
+
+  context.res = {
+    body: bookings
+  };
+};
